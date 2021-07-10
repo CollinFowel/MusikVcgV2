eeabcb5cf54e7ff92c2a5c74fc64543824e789fd
repi <71125754+CollinFowel@users.noreply.github.com>
2021-07-10 15:36:30 +0700

    Update 5.0

---
+++ MusikVcg/services/converter/__init__.py
@@ -0,0 +1,12 @@
+from os import listdir
+
+from os import mkdir
+
+if 'raw_files' not in listdir():
+
+    mkdir('raw_files')
+
+from MusikVcg.services.converter.converter import convert
+
+__all__ = ["convert"]
+
