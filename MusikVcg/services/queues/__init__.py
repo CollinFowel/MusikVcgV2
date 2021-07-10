9f955fcc67a29c9827b561c8f4ed5077710c32d3
repi <71125754+CollinFowel@users.noreply.github.com>
2021-07-10 15:40:16 +0700

    Update 5.0

---
+++ MusikVcg/services/queues/__init__.py
@@ -0,0 +1,12 @@
+from MusikVcg.services.queues.queues import clear 
+
+from MusikVcg.services.queues.queues import get
+
+from MusikVcg.services.queues.queues import is_empty
+
+from MusikVcg.services.queues.queues import put
+
+from MusikVcg.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
+
