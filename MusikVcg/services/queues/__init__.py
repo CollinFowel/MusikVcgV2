a5127dbd9856cd202724532f14634d49e24ebbcb
repi <71125754+CollinFowel@users.noreply.github.com>
2021-07-18 09:19:13 +0700

    MusikVcg V5

--- MusikVcg/services/queues/__init__.py
+++ MusikVcg/services/queues/__init__.py
@@ -1,7 +1,3 @@
-from MusikVcg.services.queues.queues import clear 
-from MusikVcg.services.queues.queues import get
-from MusikVcg.services.queues.queues import is_empty
-from MusikVcg.services.queues.queues import put
-from MusikVcg.services.queues.queues import task_done
+from MusikVcg.services.queues.queues import clear, get, is_empty, put, task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
