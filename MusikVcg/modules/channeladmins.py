8ca7d56f8ce748b56dd4bad9c7d5240261de1367
repi <71125754+CollinFowel@users.noreply.github.com>
2021-08-07 13:03:04 +0700

    Change Credits

--- MusikVcg/modules/channeladmins.py
+++ MusikVcg/modules/channeladmins.py
@@ -144,4 +144,4 @@ async def admincache(client, message: Message):
             for member in await conchat.linked_chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("✅ Bot telah aktif\n✅ Daftar admin diperbaharui!\n\n✨ ᴊᴏɪɴ ᴊᴜɢᴀ ɢᴄ ᴋᴀᴍɪ ᴅɪ @infble")
+    await message.reply_text("✅ Bot telah aktif\n✅ Daftar admin diperbaharui!\n\n✨ **Join Group kita ya @ChatBotXanon")
