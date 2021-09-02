5f441031a41b19ba38f2fa9235a7ec0967400467
repi <71125754+CollinFowel@users.noreply.github.com>
2021-09-02 11:51:02 +0700

    Fix all bugs & update

--- MusikVcg/services/downloaders/youtube.py
+++ MusikVcg/services/downloaders/youtube.py
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Modified by Inukaasith
-# REMODIFIED by CollinFowel
 
 from os import path
 
@@ -25,7 +24,8 @@ from MusikVcg.config import DURATION_LIMIT
 from MusikVcg.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio[ext=m4a]",
+    "format": "bestaudio/best",
+    "verbose": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
@@ -40,14 +40,14 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"📛 Video atau lagu dengan durasi lebih dari {DURATION_LIMIT} menit tidak dapat diputar!, "
-            f"Durasi yg diizinkan adalah {duration} menit .",
+            f"Video atau lagu dengan durasi lebih dari {DURATION_LIMIT} menit tidak dapat diputar!, "
+            f"Durasi yg diizinkan adalah {duration} menit",
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"📛 Video atau lagu dengan durasi lebih dari {DURATION_LIMIT} menit tidak dapat diputar!, "
-            f"Durasi yg diizinkan adalah {duration} menit .",
+            f"Video atau lagu dengan durasi lebih dari {DURATION_LIMIT} menit tidak dapat diputar!, "
+            f"Durasi yg diizinkan adalah {duration} menit",
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
