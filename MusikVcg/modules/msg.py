2134bbb5e5b1198fd3f88ca65d65e91925c67331
CollinFowel <71125754+CollinFowel@users.noreply.github.com>
2021-09-10 18:21:27 +0700

    lol

--- MusikVcg/modules/msg.py
+++ MusikVcg/modules/msg.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from DaisyXMusic.config import ASSISTANT_NAME, PROJECT_NAME
+from MusikVcg.config import ASSISTANT_NAME, PROJECT_NAME
 
 
 class Messages:
